168149fe3c3b2d9076e69eb922e9c2af6d2a6c59
ISmeneger <ISmeneger@yandex.ru>
2025-05-10 22:04:18 +0300

    Fix initDriver

--- src/test/java/ui/BaseTest.java
+++ src/test/java/ui/BaseTest.java
@@ -18,7 +18,7 @@ class BaseTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        initDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
     }
